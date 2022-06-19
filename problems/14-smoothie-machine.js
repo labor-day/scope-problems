8b74fbd8d7b9c92c15c39df680c5b9119b581cee
labor-day <shaffer.scott@yahoo.com>
2022-06-18 18:10:09 -0600

    completed

---
+++ problems/14-smoothie-machine.js
@@ -0,0 +1,62 @@
+/***********************************************************************
+Write a function named `smoothieMachine` that accepts any number of params and
+a function.
+
+The return function will also accept any number of params and will return a
+string including all of the parameters of smoothieMachine and the return
+function.
+
+See below for examples:
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
+***********************************************************************/
+
+// your code here
+const smoothieMachine = (...params) => {
+  let statement = "I'm having a smoothie with ";
+
+  return (...args) => {
+    let elements = [...params, ...args]
+    console.log(elements);
+    elements.forEach(
+      (element, index) => {
+        if (index === 0 && elements.length === 1) {
+          statement = statement + element;
+        } else {
+            statement = statement + " and " + element;
+        }
+
+      }
+    );
+
+    return statement;
+  }
+}
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = smoothieMachine;
+} catch (e) {
+  // catch the ref err
+  module.exports = null;
+}
